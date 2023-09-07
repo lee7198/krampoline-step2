b10d8ae93edab2f1c9597d19c2d249a6cdededc3
do98 <do98@kakao.com>
2023-09-07 22:36:38 +0900

    init

--- earth-client/vite.config.js
+++ earth-client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 const env = loadEnv( process.cwd(), '')
 
-const staticServerUri = "k31a28776702aa" || "";  
+const staticServerUri = "ka5f92745ae1ea" || "";  
 
 // https://vitejs.dev/config/
 export default defineConfig({
